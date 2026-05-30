cc1502ba6a6019ca65647579daa16a159301f803
Yintong Soong <soongyintong@outlook.com>
2026-05-30 23:47:30 +0800

    fix: add auth middleware to notification routes

--- apps/api/src/routes/notificationRoutes.js
+++ apps/api/src/routes/notificationRoutes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { getNotifications, postNotification } from "../controllers/notificationController.js";
+import { authMiddleware } from "../middleware/auth.js";
 
 export const notificationRoutes = Router();
 
+notificationRoutes.use(authMiddleware);
 notificationRoutes.get("/", getNotifications);
 notificationRoutes.post("/", postNotification);
